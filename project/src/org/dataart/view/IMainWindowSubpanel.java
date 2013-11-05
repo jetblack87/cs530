e7aadeb00e44e089dddf81cefc627416429ae389
jetblack87 <markwalbrecht@hotmail.com>
2013-11-04 21:00:34 -0500

    nightly commit 2

---
+++ project/src/org/dataart/view/IMainWindowSubpanel.java
@@ -0,0 +1,8 @@
+package org.dataart.view;
+
+import javax.swing.JMenu;
+import javax.swing.JTabbedPane;
+
+public interface IMainWindowSubpanel {
+	public void addThisPanel(JTabbedPane tabbedPane, JMenu playMenu);
+}
